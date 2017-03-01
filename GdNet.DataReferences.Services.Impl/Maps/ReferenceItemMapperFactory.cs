9948dc88f7aba79794c7125695e8f8bad7ac53bb
juanonsoftware <juanonsoftware@github.com>
2017-03-01 12:06:00 +0700

    Service 1.3.0

---
+++ GdNet.DataReferences.Services.Impl/Maps/ReferenceItemMapperFactory.cs
@@ -0,0 +1,17 @@
+﻿using GdNet.DataReferences.Domain.List;
+using GdNet.DataReferences.Services.Dtos;
+using GdNet.Layers.AutoMapper;
+
+namespace GdNet.DataReferences.Services.Impl.Maps
+{
+    public class ReferenceItemMapperFactory : DefaultEditableEntityMapperFactory<ReferenceItemDto, ReferenceItem>
+    {
+        protected override void OnMapperCreated()
+        {
+            base.OnMapperCreated();
+
+            MappingExpression.ForMember(x => x.Code, a => a.Ignore());
+            MappingExpression.ForMember(x => x.Position, a => a.Ignore());
+        }
+    }
+}
--- GdNet.DataReferences.Services/Dtos/ReferenceItemDto.cs
+++ GdNet.DataReferences.Services/Dtos/ReferenceItemDto.cs
@@ -1,13 +1,7 @@
-﻿using GdNet.Layers.Dtos;
-
-namespace GdNet.DataReferences.Services.Dtos
+﻿namespace GdNet.DataReferences.Services.Dtos
 {
-    public class ReferenceItemDto : EditableEntityDto
+    public class ReferenceItemDto : ReferenceItemBaseDto
     {
-        public string Code { get; set; }
-
-        public string Name { get; set; }
-
         public string Value { get; set; }
     }
 }
