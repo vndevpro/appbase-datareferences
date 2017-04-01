14317e4f68494c736f23eb5d7ad11e8c80e807e3
juanonsoftware <juan@vndev.top>
2017-04-01 17:42:57 +0700

    Domain V140

--- GdNet.DataReferences.Domain/IReferenceItemRepository.cs
+++ GdNet.DataReferences.Domain/IReferenceItemRepository.cs
@@ -1,4 +1,5 @@
-﻿using GdNet.DataReferences.Domain.List;
+﻿using System.Collections.Generic;
+using GdNet.DataReferences.Domain.List;
 using GdNet.Domain.Repository;
 
 namespace GdNet.DataReferences.Domain
@@ -6,5 +7,7 @@ namespace GdNet.DataReferences.Domain
     public interface IReferenceItemRepository : IRepositoryBase<ReferenceItem>
     {
         ReferenceItem GetByCode(object value);
+
+        IEnumerable<ReferenceItem> GetByCodes(params string[] codes);
     }
 }
--- GdNet.DataReferences.Domain/IReferenceListRepository.cs
+++ GdNet.DataReferences.Domain/IReferenceListRepository.cs
@@ -1,4 +1,5 @@
-﻿using GdNet.DataReferences.Domain.List;
+﻿using System.Collections.Generic;
+using GdNet.DataReferences.Domain.List;
 using GdNet.Domain.Repository;
 
 namespace GdNet.DataReferences.Domain
@@ -6,5 +7,7 @@ namespace GdNet.DataReferences.Domain
     public interface IReferenceListRepository : IRepositoryBase<ReferenceList>
     {
         ReferenceList GetByCode(object value);
+
+        IEnumerable<ReferenceList> GetByCodes(params string[] codes);
     }
 }
