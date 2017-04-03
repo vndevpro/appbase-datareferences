cf42d09b9211e88a0e0aa102d257a6071e2dca02
juanonsoftware <juanonsoftware@github.com>
2017-04-03 13:59:03 +0700

    Update mapper

--- GdNet.DataReferences.Services.Impl/Maps/ReferenceItemMapperFactory.cs
+++ GdNet.DataReferences.Services.Impl/Maps/ReferenceItemMapperFactory.cs
@@ -10,8 +10,13 @@ namespace GdNet.DataReferences.Services.Impl.Maps
         {
             base.OnMapperCreated();
 
+            // Code will never be changed
+            // Position will be updated by move up / move down action only
+            // Value will not be changed implicitly, must be updated by specific scenarios
+
             MappingExpression.ForMember(x => x.Code, a => a.Ignore());
             MappingExpression.ForMember(x => x.Position, a => a.Ignore());
+            MappingExpression.ForMember(x => x.Value, a => a.Ignore());
         }
     }
 }
